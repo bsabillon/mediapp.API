e226db1c900afa5c24e53e1c09a5b68f0747329c
bsabillon <bryan.sabillon@hotmail.com>
2020-10-22 08:18:06 -0600

    Update appointments.js

--- routes/appointments.js
+++ routes/appointments.js
@@ -6,7 +6,7 @@ const appointmentMethods = require('../services/app/appointmentsServices');
 router.post('/add', appointmentMethods.addAppointment);
 router.get('/byUserId/:userId?', appointmentMethods.getAppointments);
 router.get('/byId/:id?', appointmentMethods.getAppointmentById);
-router.get('/ifAppointment/:doctorId?/:dateId?/:hourId?/', appointmentMethods.verifyIfAppointment);
+router.get('/ifAppointment/:doctorId?/:dateId?/:hourId?', appointmentMethods.verifyIfAppointment);
 router.put('/update/:id?', appointmentMethods.updateAppointment);
 router.delete('/delete/:id?', appointmentMethods.deleteAppointment);
 
